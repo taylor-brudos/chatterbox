96c16687e8535841729cdb33c19e9472a922c021
Taylor Brudos <taylorbrudos@Taylors-MacBook-Air.local>
2018-09-26 14:11:58 -0700

    adding components

---
+++ public/src/app/chatterbox/chatterbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chatterbox',
+  templateUrl: './chatterbox.component.html',
+  styleUrls: ['./chatterbox.component.css']
+})
+export class ChatterboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var app = express()
+var bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(express.static(__dirname+'/public/dist/public'))
+require('./server/model')
+require('./server/routes')(app)
+app.listen(8000)
