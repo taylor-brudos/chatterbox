58b531fdf6901ad506a1902b5f33972a3f357eeb
Taylor Brudos <taylorbrudos@Taylors-MacBook-Air.local>
2018-09-26 15:38:33 -0700

    updates to server etc

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpService } from './http.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SplashComponent } from './splash/splash.component';
 import { CreateComponent } from './create/create.component';
 import { ChatterboxComponent } from './chatterbox/chatterbox.component';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @NgModule({
   declarations: [
@@ -18,9 +23,11 @@ import { ChatterboxComponent } from './chatterbox/chatterbox.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- server/controller.js
+++ server/controller.js
@@ -6,12 +6,82 @@ module.exports = {
     allBoxes: function(req,res) {
         Box.find({},function(err,boxes) {
             if(err) {
-                res.json({message:"Error", error:err});
+                res.json({ServerMessage:"Error", error:err});
             } else {
-                res.json({message:"Success", Boxes: boxes});
+                res.json({ServerMessage:"Success", Boxes: boxes});
             }
         })
     },
 
-    
+    createBox: function(req,res) {
+        Box.create(req.body, function(err, box) {
+            if(err) {
+                res.json({ServerMessage: "Error", Error: err})
+            } else {
+                res.json({ServerMessage: "Success", Box: box})
+            }
+        })
+    },
+
+    showBox: function(req,res) {
+        Box.findOne({_id: req.params.id}, function(err, box) {
+            if(err) {
+                res.json({ServerMessage: "Error", Error: err})
+            } else {
+                res.json({ServerMessage: "Success", Box: box})
+            }
+        })
+    },
+
+    updateBox: function(req,res) {
+        Box.findOneAndUpdate({_id: req.params.id}, req.body, {runValidators: true}, function(err) {
+            if(err) {
+                res.json({ServerMessage: "Error", Error: err})
+            } else {
+                res.json({ServerMessage: "Success"})
+            }
+        })
+    },
+
+    destroyBox: function(req,res) {
+        Box.remove({_id: req.params.id}, function(err) {
+            if(err) {
+                res.json({ServerMessage:"Error", Error: err})
+            } else {
+                res.json({ServerMessage:"Success"})
+            }
+        })
+    },
+
+    createMessage: function(req,res) {
+        Message.create(req.body, function(err, message) {
+            if(err) {
+                res.json({ServerMessage:"Error", Error: err})
+            } else {
+                Box.findOneAndUpdate({_id: req.params.id}, {$push: {messages: message}}, function(err, box) {
+                    if(err) {
+                        res.json({ServerMessage:"Error", Error: err})
+                    } else {
+                        res.json({ServerMessage:"Success", Box: box})
+                    }
+                })
+            }
+        })
+    },
+
+    deleteMessage: function(req,res) {
+        Message.remove({_id: req.params.sid}, function(err) {
+            if(err) {
+                res.json({ServerMessage: "Error", Error: err})
+            } else {
+                Box.findOneAndUpdate({_id:req.params.id}, {$pull: {messages:{_id:req.params.sid}}}, function(err, box) {
+                    if(err) {
+                        res.json({ServerMessage: "Error", Error: err})
+                    } else {
+                        res.json({ServerMessage: "Success", Box: box})
+                    }
+                })
+            }
+        })
+    }
 }
